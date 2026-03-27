59535978412726fd4a4a13764b7a926abd91f55a
Connor Bell (Makeshift) <git@connor-bell.com>
2026-03-27 16:21:26 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# github-api-json-schema
+
+Converts operations within GitHub's [OpenAPI spec](https://github.com/github/rest-api-description/) to JSON schemas.
+
+Schemas are automatically re-generated every Tuesday via GitHub Actions.
---
+++ generate.ts
@@ -0,0 +1,70 @@
+import SwaggerParser from '@apidevtools/swagger-parser'
+import { openapiSchemaToJsonSchema } from '@openapi-contrib/openapi-schema-to-json-schema'
+import { OpenAPIV3 } from 'openapi-types'
+import { Octokit } from 'octokit'
+
+const octokit = new Octokit()
+const files = await octokit.rest.repos.getContent({
+  owner: 'github',
+  repo: 'rest-api-description',
+  path: 'descriptions/api.github.com/'
+})
+
+if (!Array.isArray(files.data)) throw new Error('Expected directory listing')
+  
+const versions = files.data.filter(file => file.name.startsWith('api.github.com.') && file.name.endsWith('.json'))
+
+for (const version of versions) {
+  let versionName = version.name.replace('api.github.com.', '')
+  if (versionName === 'json') {
+    versionName = 'latest'
+  } else {
+    versionName = versionName.replace('.json', '')
+  }
+  
+  console.log(`Processing version ${version.name} as ${versionName}`)
+  
+  const parser = new SwaggerParser()
+  const api = await parser.dereference(version.download_url!) 
+
+  const waits = []
+  const namespaces = new Set<string>()
+
+  for (const [path, pathItem] of Object.entries(api.paths ?? {})) {
+    if (!pathItem) {
+      continue
+    }
+
+    const operationsByMethod = pathItem as Partial<Record<OpenAPIV3.HttpMethods, OpenAPIV3.OperationObject>>
+
+    for (const method of Object.values(OpenAPIV3.HttpMethods)) {
+      const operation = operationsByMethod[method]
+      if (!operation) {
+        continue
+      }
+
+      if (!operation.operationId) {
+        throw new Error(`operationId is required for ${method} ${path}`)
+      }
+
+      const requestBody = operation.requestBody
+      if (!requestBody || '$ref' in requestBody) {
+        continue
+      }
+
+      const namespace = operation.operationId.split('/')[0]!
+      if (!namespaces.has(namespace)) {
+        namespaces.add(namespace)
+        console.log(`Processing namespace ${namespace} in ${versionName}`)
+      }
+      
+      const requestType = requestBody.content['application/json']?.schema
+      if (requestType) {
+        const asJsonSchema = openapiSchemaToJsonSchema(requestType)
+        waits.push(Bun.write(`./schemas/${versionName}/${operation.operationId}.json`, JSON.stringify(asJsonSchema, null, 2)))
+      }
+    }
+  }
+
+  await Promise.all(waits)
+}
